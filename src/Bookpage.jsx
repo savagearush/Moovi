95b57fa5fcbb6ed557e2310922d66cf14068f9a1
Arush Sharma <arush3339@gmail.com>
2023-05-04 21:03:03 +0530

    All Done

--- src/Bookpage.jsx
+++ src/Bookpage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import "./bookpage.css";
 import { MovieContext } from "./movieContext";
 import notFound from "./assets/placeholder.jpg";
@@ -6,6 +6,9 @@ import { Link } from "react-router-dom";
 
 export const Bookpage = () => {
   const { currentMovie } = useContext(MovieContext);
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const closeBtn = useRef();
 
   let movie = {
     name: "Not Found",
@@ -16,9 +19,17 @@ export const Bookpage = () => {
     summary: "Not Found",
   };
 
-  if (currentMovie !== null) {
-    movie = currentMovie[0].show;
-  }
+  if (currentMovie !== null) movie = currentMovie[0].show;
+
+  const handleSubmit = () => {
+    let user = {
+      userName,
+      email,
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+    closeBtn.current.click();
+  };
 
   return (
     <div className="screen2">
@@ -54,7 +65,78 @@ export const Bookpage = () => {
         </h6>
         <div className="movie-summary lead">{movie.summary}</div>
 
-        <button className="book-btn">Book Ticket</button>
+        <button
+          className="book-btn"
+          data-bs-toggle="modal"
+          data-bs-target="#bookMovieTicket"
+        >
+          Book Ticket
+        </button>
+      </div>
+
+      <div
+        className="modal fade"
+        id="bookMovieTicket"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Book Tickets For : {movie.name}
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <label for="name" class="form-label">
+                  Name :
+                </label>
+                <input
+                  type="Text"
+                  id="name"
+                  class="form-control"
+                  aria-labelledby="passwordHelpBlock"
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <label for="name" class="form-label">
+                  Email Address :
+                </label>
+                <input
+                  type="email"
+                  id="name"
+                  class="form-control"
+                  aria-labelledby="passwordHelpBlock"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                ref={closeBtn}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,7 +4,9 @@ import "./homepage.css";
 export const Homepage = () => {
   return (
     <div className="screen1">
-      <h2 className="text-center m-3 text-white"> Book Your Movie Tickets</h2>
+      <h2 className="text-center m-3 text-white logo">
+        Moovi - Book Movie Tickets
+      </h2>
       <div className="movies">
         <Movies />
       </div>
